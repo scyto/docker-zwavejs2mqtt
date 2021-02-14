06a771357826c9f6a171b584baf631cd2cf4af52
scyto <lx_b@msn.com>
2021-02-13 23:44:45 -0800

    Delete build.ps1
    unneeded file

--- build.ps1
+++
@@ -1,1 +0,0 @@
-docker buildx build  --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg SRC=git-clone-src --build-arg Z2M_BRANCH=master --build-arg ZWJ_BRANCH=master  --push  -f ./docker/Dockerfile.contrib -t scyto/zwavejs2mqtt:master https://github.com/zwave-js/zwavejs2mqtt.git
